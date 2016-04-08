695a1568ea8afe93d1e21cf414e549a1b49f7e5c
AlexL29 <alela1516@st-2249ga.sesame-st.org>
2016-04-08 09:27:11 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Download More Air
+=================
+
+Click [here][1] to visit Download More Air.
+
+License: [WTFPL][2]
+
+[1]: https://downloadmoreair.github.io/
+[2]: http://www.wtfpl.net/about/
---
+++ script.js
@@ -0,0 +1,28 @@
+// License: see COPYING
+
+var progress = document.getElementById("progress");
+
+var clicked = false;
+var percent = 0;
+
+document.getElementById("download").onclick = function () {
+  if (!clicked) {
+    clicked = true;
+    progress.innerHTML = String(percent) + "% complete ";
+    window.setInterval(function () {
+      percent += 1;
+      progress.innerHTML = String(percent) + "% complete ";
+      if (percent === 100) {
+        window.alert("Your download is complete");
+      }
+      if (percent === 1000) {
+        window.alert("You can stop now");
+      }
+      if (percent === 9001) {
+        window.alert("IT'S OVER 9000!");
+      }
+    }, 100);
+  } else {
+    percent += 1;
+  }
+};
